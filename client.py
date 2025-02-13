2ba8c11eb69ed18db3acfaaa33685e463fa59d3a
ASHISH PRAJAPATI <ashishofficial12321@gmail.com>
2025-02-13 16:09:51 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,34 +1,7 @@
-# from openai import OpenAI
- 
-# # pip install openai 
-# # if you saved the key under a different environment variable name, you can do something like:
-# client = OpenAI(
-#   api_key="Enter your openai api",
-# )
-
-# completion = client.chat.completions.create(
-#   model="gpt-3.5-turbo",
-#   messages=[
-#     {"role": "system", "content": "You are a virtual assistant named jarvis skilled in general tasks like Alexa and Google Cloud"},
-#     {"role": "user", "content": "what is coding"}
-#   ]
-# )
-
-# print(completion.choices[0].message.content)
-
-
-
-
-
-
-
-
-
-
 import google.generativeai as genai
 
 # Configure Gemini AI with your API key
-genai.configure(api_key="AIzaSyCvGIIAJAW43kdC2x0EUAU_6q9tFmNSxdw")
+genai.configure(api_key="gemini api")
 
 # Initialize the Gemini AI model
 model = genai.GenerativeModel("gemini-pro")
